af4c0416f14c2360cd37415c6ae3d55dbf4d807e
Igor Semin <isemin@outlook.com>
2015-01-06 23:19:30 +0300

    * cross-browser done!

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,44 @@
-$(document).ready(function(){$('.carousel').carousel({interval:false});
+$(document).ready(function() {
+
+  var $j = jQuery.noConflict();
+
+  $j('.carousel').carousel({
+    interval:false
+  });
 
   /* affix the navbar after scroll below header */
-  $('#nav').affix({
+  $j('#nav').affix({
         offset: {
-          top: $('header').height()-$('#nav').height()
+          top: $j('header').height()-$j('#nav').height()
         }
   });	
 
   /* highlight the top nav as scrolling occurs */
-  $('body').scrollspy({ target: '#nav' })
+  $j('body').scrollspy({ target: '#nav' })
 
   /* smooth scrolling for scroll to top */
-  $('.scroll-top').click(function(){
-    $('body,html').animate({scrollTop:0},1000);
+  $j('.scroll-top').click(function(){
+    $j('body,html').animate({scrollTop:0},1000);
   })
 
   /* smooth scrolling for nav sections */
-  $('#nav .navbar-nav li>a').click(function(){
-    var link = $(this).attr('href');
-    var posi = $(link).offset().top;
-    $('body,html').animate({scrollTop:posi},700);
+  $j('#nav .navbar-nav li>a').click(function(){
+    var link = $j(this).attr('href');
+    var posi = $j(link).offset().top;
+    $j('body,html').animate({scrollTop:posi},700);
   });
 
 
   /* copy loaded thumbnails into carousel */
-  $('.panel .img-responsive').on('load', function() {
+  $j('.panel .img-responsive').on('load', function() {
     
   }).each(function(i) {
-    	var item = $('<div class="item"></div>');
-      var itemDiv = $(this).parent('a');
-      var title = $(this).parent('a').attr("title");
+    	var item = $j('<div class="item"></div>');
+      var itemDiv = $j(this).parent('a');
+      var title = $j(this).parent('a').attr("title");
       
       item.attr("title",title);
-    	$(itemDiv.html()).appendTo(item);
+    	$j(itemDiv.html()).appendTo(item);
     	item.appendTo('#modalCarousel .carousel-inner'); 
 
       if (i==0){ // set first item active
@@ -41,23 +47,22 @@ $(document).ready(function(){$('.carousel').carousel({interval:false});
   });
 
   /* activate the carousel */
-  $('#modalCarousel').carousel({interval:false});
+  $j('#modalCarousel').carousel({interval:false});
 
   /* change modal title when slide changes */
-  $('#modalCarousel').on('slid.bs.carousel', function () {
+  $j('#modalCarousel').on('slid.bs.carousel', function () {
     //$('.modal-title').html($(this).find('.active').attr("title"));
   })
 
   /* when clicking a thumbnail */
-  $('.panel-thumbnail>a').click(function(e){
+  $j('.panel-thumbnail>a').on("click", function(e){
     
       e.preventDefault();
-      var id = parseInt($(this).parents('.panel').parent().index());
-    	
-      jQuery.noConflict();
+      var id = parseInt($j(this).parents('.panel').parent().index());
+    
+    	$j('#photoProj').modal('show'); // show the modal
+      $j('#modalCarousel').carousel(id); // slide carousel to selected
 
-    	$('#myModal').modal('show'); // show the modal
-      $('#modalCarousel').carousel(id); // slide carousel to selected
     	return false;
   });
 });
